adebc6082fd084a659a1ccc9e5796b243ac9004a
swJaNG12 <cbdjej38483@gmail.com>
2023-06-02 15:47:55 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-
-searchEl.addEventListener('click',() => {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', () => {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', () => {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-})
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 window.addEventListener('scroll', _.throttle(() => {
@@ -140,7 +121,3 @@ spyEls.forEach(function(spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 })
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear()
